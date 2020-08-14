04ceb593bb48a2523e0dcb09c4c9d71387af4820
Naman Goyal <namangoyal4900@gmail.com>
2020-08-14 23:02:41 +0530

    intro/about section complete

---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./About.scss";
+
+import my_pic from "../../Assets/my_pic.png";
+
+const About = () => {
+  return (
+    <div className="about-section">
+      <div className="left">
+        <p className="upper">
+          Hi, I’m <span>Naman.</span>
+        </p>
+        <p className="lower">
+          I am currently pursuing my Bachelors of Technolody in Information
+          Technology, and I love to design and develop websites.{" "}
+          <span>I strive to make web a beautiful place.</span>
+        </p>
+      </div>
+      <div className="right">
+        <img src={my_pic} alt="my pic"></img>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-import './App.scss'
+import "./App.scss";
 
 //import all components here
 import Header from "./Header/Header";
+import About from "./About/About";
 
 function App() {
   return (
     //This fragment should contain all the child components
     <>
       <Header />
+      <About />
     </>
   );
 }
